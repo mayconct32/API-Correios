0937a34761ae074da6f7ce055071281fa10db868
mayconct32 <maycon326754@gmail.com>
2026-03-12 16:46:09 -0300

    test: adiciona testes para o caminho '/zip/'

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -12,3 +12,37 @@ async def client():
     ) as client:
         yield client
 
+
+@pytest.mark.asyncio
+async def test_get_by_zip_code(client):
+    zipcode = "36803000"
+    response = await client.get(f"/zip/{zipcode}")
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "state": "MG",
+        "city": "Carangola",
+        "Neighborhood": "Alvorada",
+        "zipcode": "36803000",
+        "street": ""
+    }
+
+
+@pytest.mark.asyncio
+async def test_get_by_zip_code_empty_validation(client):
+    response = await client.get("/zip/")
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {
+        "detail": "CEP não pode ser vazio"
+    }
+
+
+@pytest.mark.asyncio
+async def test_get_by_zip_code_404(client):
+    zipcode = "00000000"
+    response = await client.get(f"/zip/{zipcode}")
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {
+        "detail": f"CEP {zipcode} não encontrado"
+    }
+
+
